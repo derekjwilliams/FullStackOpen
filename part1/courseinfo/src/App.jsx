49a06c8812b0d2d3c0e6778cfb63cc9484ccadc7
derek williams <derek61@gmail.com>
2023-10-15 11:31:04 -0600

    move components from files into App.jsx to simplfy

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,35 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.course.parts[0].exercises +
+        props.course.parts[1].exercises +
+        props.course.parts[2].exercises}
+    </p>
+  );
+};
 
 const App = () => {
   const course = {
