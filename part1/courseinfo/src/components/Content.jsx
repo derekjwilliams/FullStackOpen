cfa3e53a73bec4dee0669b93cc78ba66486ece80
derek williams <derek61@gmail.com>
2023-10-15 10:57:11 -0600

    part 1.4 complete

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,10 @@
+import Part from './Part'
 const Content = (props) => {
   return (
     <>
-    <p>
-      {props.part1} {props.exercises1} 
-    </p>
-    <p>
-      {props.part2} {props.exercises2} 
-    </p>
-    <p>
-      {props.part3} {props.exercises3} 
-    </p>
+    <Part part={props.parts[0]}/>
+    <Part part={props.parts[1]}/>
+    <Part part={props.parts[2]}/>
     </>
   )
 }
